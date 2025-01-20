93ea20e0d2dccd0b99bba5af98fd5b1d3ea433ae
jo <129003967+jolateral@users.noreply.github.com>
2025-01-20 17:05:30 -0500

    finished the game!

--- main.py
+++ main.py
@@ -8,40 +8,73 @@ word_list = ["apple", "quantum", "umbrella", "spider", "orange"]
 
 """This function is the start to the game."""
 def run_game() -> None:
-    # choose the answer
+    # set up the lives and if the player has won
+    lives = 5
+    status = "lose"
+
+    # initiate the answer
     answer = random.choice(word_list)
-
-    # initiate the player's answer list
-    guess_so_far = []
+    answer_list = []
     for i in range(len(answer)):
-        guess_so_far.append("_")
+        answer_list.append(answer[i])
 
-    # start the game!
-    print("Let's play Hangman! To guess a word, type 'word'. If you want to guess a letter, type 'letter.")
+    # initiate the player's progress
+    progress = []
+    for i in range(len(answer)):
+        progress.append('_')
 
-    action = input()
+    # introduce the game
+    print("Hello, let's play Hangman!")
 
-    if action == "letter":  # the player guesses a letter
-        print("What letter would you like to guess?")
-        guess = input()
+    while lives != 0 and (progress != answer_list):
+        print('Current progress:', progress)
+        print("Lives remaining:", lives)
+        # let the player choose what action to do
+        print('Would you like to guess a letter or a word?')
+        action = input()
+        
+        if action == 'letter':
+            # the player wants to guess a letter
+            print("What leter would you like to guess?")
+            guess = input()
 
-        if guess in answer:
-            counter = 0
-            for i in answer:
-                if i == guess:
-                    guess_so_far[counter] = guess
-                counter += 1
+            if guess in answer:
+                # the letter is in the answer!
+                # update progress
+                for i in range(len(answer)):
+                    if guess == answer_list[i]:
+                        progress[i] = guess
+
+                print("Good job! The letter is in the word")
+                continue
+
+            else:
+                # the letter is not in the answer
+                print("Sorry! That's not in the answer")
+                lives -= 1
+
+        elif action == 'word':
+            print("What word would you like to guess?")
+            guess = input()
+
+            if answer == guess:
+                print("Good job! You won the game!")
+                progress = answer_list
+                break
             
-            print("Good job! Here's your progress so far:")
-            print(guess_so_far)
+            else:
+                print("Sorry, that's not the answer!")
+                lives -= 1
 
-    elif action == "word":
-        print("What word would you like to guess?")
-        guess = input()
+        else:
+            print("Thats not a valid answer! You must type either 'letter' or 'word")
+            continue
 
-        # compare the guess with the answer
-        if guess == answer:
-            print("You got it! The answer was " + answer)
-            return
-        else:   # the guess is not correct
-            ...
+    if progress == answer_list:
+        print("Congrats, you won the game!!")
+    if progress != answer_list:
+        print("Sorry, you lost :(")
+            
+            
+
+        
